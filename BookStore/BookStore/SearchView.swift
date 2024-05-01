6c4a81705eb237ba62583546cd9ec286ea986b4d
우유차 <142779268+labydin@users.noreply.github.com>
2024-05-01 17:57:02 +0900

    [Add] 프로젝트 파일 생 #1

---
+++ BookStore/BookStore/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  BookStore
+//
+//  Created by 박준영 on 5/1/24.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
